67f717b0ee1aa66e0a03b80fe955ddecdc8a640e
Yacine-Amouche <yacine.amouche@yahoo.fr>
2020-01-05 04:17:28 +0100

    Initial commit

---
+++ src/main/java/org/sid/cinema/dao/SalleEntity.java
@@ -0,0 +1,31 @@
+package org.sid.cinema.dao;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class SalleEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private int nombrePlace;
+	@ManyToOne
+	private CinemaEntity cinema;
+
+}
