dfc2e36a830e3036f64cd69b5cc302771a148dfa
Yacine-Amouche <yacine.amouche@yahoo.fr>
2020-01-06 00:44:36 +0100

    add the entity in villeEntity and add in the prorerties mySql

--- src/main/java/org/sid/cinema/dao/VilleEntity.java
+++ src/main/java/org/sid/cinema/dao/VilleEntity.java
@@ -1,5 +1,30 @@
 package org.sid.cinema.dao;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class VilleEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double longitude, latitude, altitude;
+	@OneToMany(mappedBy = "ville")
+	private Collection<CinemaEntity> cinemas;
 
 }
