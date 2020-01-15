bbd8232538fd5083da7d0ca853d5f67c348eb6b3
Yacine-Amouche <yacine.amouche@yahoo.fr>
2020-01-15 01:25:12 +0100

    add the entity in villeEntity and add in the prorerties mySql

--- src/main/java/org/sid/cinema/web/CinemaRestController.java
+++ src/main/java/org/sid/cinema/web/CinemaRestController.java
@@ -4,15 +4,20 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.sid.cinema.dao.FilmRepository;
+import org.sid.cinema.dao.TicketRepository;
 import org.sid.cinema.entity.Film;
 import org.sid.cinema.entity.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +28,8 @@ import lombok.Data;
 public class CinemaRestController {
 	@Autowired
 	private FilmRepository filmRepository ;
+	@Autowired
+	private TicketRepository ticketRepository ;
 	@GetMapping(path = "/imageFilm/{id}",produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] image(@PathVariable(name ="id") Long id) throws Exception{
 		Film f =filmRepository.findById(id).get();
@@ -31,13 +38,23 @@ public class CinemaRestController {
 		Path path = Paths.get(file.toURI());
 		return Files.readAllBytes(path);
 	}
-	public List<Ticket>peyerTickets (@RequestBody TicketFrom ticketFrom)  {
-		
+	@PostMapping("/payerTickets")
+	public List<Ticket> peyerTickets (@RequestBody TicketFrom ticketFrom)  {
+		List<Ticket> listTicket = new ArrayList<>();
+		ticketFrom.getTickets().forEach(idTicket ->{
+			Ticket ticket=ticketRepository.findById(idTicket).get();
+			ticket.setNomClien(ticketFrom.getNomClient());
+			ticket.setReserve(true);
+			ticketRepository.save(ticket);
+			
+		});
+		return listTicket;
 	}
 	
 }
 @Data
 class TicketFrom {
-	
+	private String nomClient;
+	private List<Long> tickets= new ArrayList<>();
 	
 }
